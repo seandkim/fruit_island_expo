c4af52f6863b1845768c27a5650c052c59935dcc
Sean Kim <seandkim14@gmail.com>
2017-09-18 21:44:11 -0400

    copied old fruit island

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import CameraScreen from './src/components/CameraScreen';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './src/reducers';
+import Game from './src/components/Game';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
   render() {
+    // initial state
+    const init = {
+      "currentStageIdx": 0,
+    }
+
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <CameraScreen />
-      </View>
+      <Provider store={createStore(reducers, init)}>
+        <View style={styles.container}>
+          <Game/>
+        </View>
+      </Provider>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
